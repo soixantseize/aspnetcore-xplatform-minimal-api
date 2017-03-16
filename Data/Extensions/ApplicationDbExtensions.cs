83ae900b3d4d2ecae96aebcb78a26ed0ac5c3dae
ReketeBravo <herman.toro@outlook.com>
2017-03-16 15:06:17 -0400

    Updated db seeding script

--- Data/Extensions/ApplicationDbExtensions.cs
+++ Data/Extensions/ApplicationDbExtensions.cs
@@ -14,11 +14,11 @@ namespace AspNetCoreBlogService.Data.Extensions
                 if (!context.BlogArticles.Any())
                 {
                     context.BlogArticles.AddRange(
-                        new BlogArticle { ArticleTitle = "Google", ArticleContent = "Google" },
-                        new BlogArticle { ArticleTitle = "Microsoft", ArticleContent = "Google"  },
-                        new BlogArticle { ArticleTitle = "Facebook", ArticleContent = "Google"  },
-                        new BlogArticle { ArticleTitle = "Salesforce", ArticleContent = "Google"  },
-                        new BlogArticle { ArticleTitle = "Oracle", ArticleContent = "Google"  });
+                        new BlogArticle { ArticleTitle = "How to Dab", ArticleContent = "First tuck you head down" },
+                        new BlogArticle { ArticleTitle = "How to Whip", ArticleContent = "Rock back and forth"  },
+                        new BlogArticle { ArticleTitle = "How to Nae Nae", ArticleContent = "Add a connecting move"  },
+                        new BlogArticle { ArticleTitle = "How to Dougie", ArticleContent = "Pass your hand through"  },
+                        new BlogArticle { ArticleTitle = "How to Wop", ArticleContent = "Worm your upper body"  });
 
                     context.SaveChanges();
                 }
